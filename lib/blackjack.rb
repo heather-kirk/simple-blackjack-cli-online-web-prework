5061022815db9cd5cb7e9e62b9dd9dea517d84cd
Heather Kirk <hskirk@gmail.com>
2019-01-07 00:18:47 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,7 @@ end
 def initial_round
   deal_card
   deal_card
-  display_card_total(total = deal_card + deal_card)
+  total = deal_card+ deal_card+ display_card_total(total)
 
   "Your cards add up to #{total}"
 end
